cbe2112c16d488c52ce396229f39fc37e886deeb
Kenneth Uy <missingno15@gmail.com>
2017-06-16 01:10:49 -0400

    Untested refactor
    * Broke out the common function of displaying comments into functions
    * Took out Stream.drop_while/2 since my guess is that there is something going that I am not quite understanding with it but I don't know what it is yet
    * Therefore, I broke that into a function as well which decides whether or not to print it or do nothing
    * There are no desirable SHOWROOM livestreams to test this on at the time of this commit so I can't say if any of this actually works
    * Also wrapped track/1 in a case statement just in case so that I know whether or not there are errors in hitting the API too fast

--- showroom_commentlog/lib/showroom/comment_log.ex
+++ showroom_commentlog/lib/showroom/comment_log.ex
@@ -1,4 +1,4 @@
-require IEx
+# require IEx
 defmodule Showroom.CommentLog do
   @moduledoc """
   Documentation for Showroom.CommentLog.
@@ -7,49 +7,46 @@ defmodule Showroom.CommentLog do
   @comment_endpoint "https://www.showroom-live.com/api/live/comment_log"
 
   def track(room_id, pointer \\ nil) do
-    response = HTTPoison.get!("#{@comment_endpoint}?room_id=#{room_id}")
-    json = Poison.decode!(response.body)
+    case HTTPoison.get!("#{@comment_endpoint}?room_id=#{room_id}") do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        response = 
+          json = Poison.decode!(response.body)
 
-    comments = json["comment_log"] 
+          comments = json["comment_log"] 
+          [most_recent_comment | _tail] = comments
 
-    [most_recent_comment | _tail] = comments
+          display_comments(pointer)
 
-    IEx.pry
-
-    if pointer == nil do
-
-      Enum.reverse(comments)
-      |> Stream.drop_while(fn comment -> 
-        String.match?(comment["comment"], ~r/^([0-4]?[0-9]|[０-４]?[０-９]|50|５０)$/u)
-      end)
-      |> Stream.each(fn comment ->
-        IO.puts("#{comment["name"]}: #{comment["comment"]}")
-      end)
-      |> Stream.run()
-      
-    else
-      # since Enum.find_index/2 is index based, the index value
-      # itself gets us the correct amount of latest comments with
-      # {Enum,Stream}.take/2
-      index_of_last_latest_comment = Enum.find_index(comments, &(&1 == pointer))
-
-      Enum.reverse(comments)
-      |> Stream.take(index_of_last_latest_comment) 
-      |> Stream.drop_while(fn comment -> 
-        String.match?(comment["comment"], ~r/^([0-4]?[0-9]|[０-４]?[０-９]|50|５０)$/u)
-      end)
-      |> Stream.each(fn comment ->
-        IO.puts("#{comment["name"]}: #{comment["comment"]}")
-      end)
-      |> Stream.run()
-
+          track(room_id, most_recent_comment)
+      {_, response} ->
+        # if its not a 200 response, just inspect it
+        IO.inspect(response)
+        :timer.sleep(:timer.seconds(1))
+        track(room_id, pointer)
     end
-
-    :timer.sleep(:timer.seconds(1))
-    track(room_id, most_recent_comment)
   end
 
-  # defp matches_count_number?(comment) do
-  #   String.match?(comment["comment"], ~r/^([0-4]?[0-9]|[０-４]?[０-９]|50|５０)$/u)
-  # end
+  defp display_comments(nil) do
+    Enum.reverse(comments)
+    |> Stream.each(&(&display_comment/1))
+    |> Stream.run()
+  end
+  defp display_comments(pointer) do
+    # since Enum.find_index/2 is index based, the index value
+    # itself gets us the correct amount of latest comments with
+    # {Enum,Stream}.take/2
+    index_of_last_latest_comment = Enum.find_index(comments, &(&1 == pointer))
+
+    Enum.reverse(comments)
+    |> Stream.take(index_of_last_latest_comment) 
+    |> Stream.each(&(&display_comment/1))
+    |> Stream.run()
+  end
+
+  def display_comment(%{"comment" => comment, "name" => user}) do
+    case String.match?(comment, ~r/^([0-4]?[0-9]|[０-４]?[０-９]|50|５０)$/u) do
+      true -> nil
+      _ -> IO.puts("#{user}: #{comment}")
+    end
+  end
 end
