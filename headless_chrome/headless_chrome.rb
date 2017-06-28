cbf2d1a83ef64e0560d43531dd1476078e666d28
Kenneth Uy <missingno15@gmail.com>
2017-06-28 03:50:36 -0400

    Tried a bunch of stuff, didnt work
    Tried a bunch of variations but it kept launching an actual browser instance instead of being headless which is supposed to be the entire point of a headless browser.

--- headless_chrome/headless_chrome.rb
+++ headless_chrome/headless_chrome.rb
@@ -2,18 +2,38 @@ require "capybara"
 require "selenium-webdriver"
 require "pry"
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
+# Capybara.register_driver :chrome do |app|
+#   Capybara::Selenium::Driver.new(app, browser: :chrome)
+# end
+
+# Capybara.register_driver :headless_chrome do |app|
+#   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+#     chromeOptions: { args: ["no-sandbox", "headless", "disable-gpu"]}
+#   )
+
+#   Capybara::Selenium::Driver.new app,
+#     browser: :chrome,
+#     desired_capabilities: capabilities
+# end
+# Capybara.register_driver :headless_chrome do |app|
+#   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+#     chromeOptions: {
+#       args: %w[ no-sandbox headless disable-gpu ]
+#     }
+#   )
+
+#   Capybara::Selenium::Driver.new(app, browser: :chrome, desired_capabilities: capabilities)
+# end
+
+# Capybara.default_driver = :headless_chrome
+
+chrome_options = {
+  browser: :chrome,
+  options: Selenium::WebDriver::Options.new(args: ["no-sandbox", "headless" ])
+}
 
 Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: ["no-sandbox", "headless", "disable-gpu"]}
-  )
-
-  Capybara::Selenium::Driver.new app,
-    browser: :chrome,
-    desired_capabilities: capabilities
+  Capybara::Selenium::Driver.new(app, chrome_options)
 end
 
 Capybara.default_driver = :headless_chrome
