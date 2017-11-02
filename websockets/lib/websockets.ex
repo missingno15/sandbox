0fae5a124a3ff34184a961c1480a680d82b4a8d9
Kenneth Uy <missingno15@gmail.com>
2017-11-02 01:52:10 -0400

    Create implementation to count comments per seconds

--- websockets/lib/websockets.ex
+++ websockets/lib/websockets.ex
@@ -1,3 +1,4 @@
+require IEx
 defmodule Websockets do
   require Logger
 
@@ -9,24 +10,52 @@ defmodule Websockets do
     socket |> Socket.Web.send!( {:text, "SUB\t#{fetch_bcsvr_key(room_url_key)}"})
     socket |> Socket.Web.send!({:text, "PING\tshowroom"})
 
-    loop(socket, Timex.now, 0)
+    loop(socket, Timex.now, 0, 0)
   end
 
-  defp loop(socket, _ping_time, time_elapsed_since_last_ping) when time_elapsed_since_last_ping >= 60 do
+  defp loop(socket, _ping_time, time_elapsed_since_last_ping, comment_count) when time_elapsed_since_last_ping >= 60 do
     socket |> Socket.Web.send!({:text, "PING\tshowroom"})
 
-    {:text, message} = socket |> Socket.Web.recv!
-    Logger.info(message)
+    {:text, message} = socket |> Socket.Web.recv!()
+    message 
+    |> process_message()
+    |> IO.inspect()
 
-    loop(socket, Timex.now, 0) 
+    Logger.info("#{comment_count} per minute")
+
+    loop(socket, Timex.now, 0, 0) 
   end
-  defp loop(socket, ping_time, _time_elapsed_since_last_ping) do
-    {:text, message} = socket |> Socket.Web.recv!
-    Logger.info(message)
+  defp loop(socket, ping_time, _time_elapsed_since_last_ping, comment_count) do
+    {:text, message} = socket |> Socket.Web.recv!()
 
-    loop(socket, ping_time, Timex.diff(Timex.now, ping_time, :seconds))
+
+    message 
+    |> process_message()
+    |> IO.inspect()
+
+    new_comment_count = message 
+                        |> process_message()
+                        |> increment_comment_count(comment_count)
+
+    # loop(socket, ping_time, Timex.diff(Timex.now, ping_time, :seconds), 0)
+    loop(socket, ping_time, Timex.diff(Timex.now, ping_time, :seconds), new_comment_count)
   end
 
+  defp process_message(message)  do
+    if String.match?(message, ~r/^ACK/) do
+      %{} 
+    else
+      message
+      |> String.split(~r/[[:space:]]/i, parts: 3)
+      |> List.last()
+      |> String.trim_leading("\"")
+      |> String.trim_trailing("\"")
+      |> Poison.decode!()
+    end
+  end
+
+  defp increment_comment_count(%{"cm" => _cm}, comment_count), do: comment_count + 1
+  defp increment_comment_count(_, comment_count), do: comment_count
 
   defp fetch_bcsvr_key(room_url_key) do
     case HTTPoison.get("#{@room_status_api_endpoint}?room_url_key=#{room_url_key}") do
